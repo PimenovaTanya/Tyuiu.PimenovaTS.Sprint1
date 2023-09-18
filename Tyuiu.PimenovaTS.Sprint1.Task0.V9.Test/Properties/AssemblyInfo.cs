32b706cc2cd8a603c99a5cbf1c4b4334fa25fbab
tyuiu.pimenovaTS@outlook.com <Tatyana.filia.2005.pimenova@mail.ru>
2023-09-18 15:36:35 +0500

    Add project files.

---
+++ Tyuiu.PimenovaTS.Sprint1.Task0.V9.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PimenovaTS.Sprint1.Task0.V9.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PimenovaTS.Sprint1.Task0.V9.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("39897307-a762-481a-899e-28b21ef24cb2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PimenovaTS.Sprint1.Task0.V9/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PimenovaTS.Sprint1.Task0.V9.Lib;
+namespace Tyuiu.PimenovaTS.Sprint1.Task0.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Пименова Т. С. | ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы C#                                          *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнила: Пименова Т. С. | ИИПб-23-2                                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать пррограмму, которая вычисляет выражение (9 / 3) * 4 / 3 / 2 + 1*");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* (9 / 3) * 4 / 3 / 2 + 1                                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+        }
+    }
+}
