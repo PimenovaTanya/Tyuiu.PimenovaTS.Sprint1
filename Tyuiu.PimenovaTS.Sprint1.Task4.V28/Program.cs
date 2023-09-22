8f15811204fa8da77c8b20578a2f2c5a05a4fca1
tyuiu.pimenovaTS@outlook.com <Tatyana.filia.2005.pimenova@mail.ru>
2023-09-22 21:33:25 +0500

    Task4

--- Tyuiu.PimenovaTS.Sprint1.Task4.V28/Program.cs
+++ Tyuiu.PimenovaTS.Sprint1.Task4.V28/Program.cs
@@ -11,6 +11,39 @@ namespace Tyuiu.PimenovaTS.Sprint1.Task4.V28
     {
         static void Main(string[] args)
         {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Пименова Т. С. | ИИПб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Class Math                                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнила: Пименова Т. С. | ИИПб-23-2                                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле  cos(60 * pi / 2) / Exp(2 * x + y) и     *");
+            Console.WriteLine("* печатает его на экране.                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x, y;
+
+            Console.WriteLine("Введите значение X: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение Y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("cos(60 * pi / 2) / Exp(2 * x + y) = " + ds.Calculate(x, y));
+
+            Console.ReadLine();
         }
     }
 }
